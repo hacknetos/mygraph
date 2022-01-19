7a8c98296fb7a124ec7082dcb90b356cba8f850a
hacknetos <76149789+hacknetos@users.noreply.github.com>
2022-01-19 11:42:01 +0100

    Primarsstage

--- src/Gui/MainGui.java
+++ src/Gui/MainGui.java
@@ -1,15 +1,33 @@
 package Gui;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainGui extends Application {
     @Override
     public void start(Stage primaryStage){
+        Group grupe = new Group();
+        grupe.getChildren().addAll(buildMenuBar(),buildNewScene());
+
         primaryStage.sizeToScene();
         primaryStage.setTitle("graphs");
-        primaryStage.setScene(new Scene(/*TODO new scen  */));
+        primaryStage.setScene(new Scene(grupe));
         primaryStage.show();
+    }
+
+    private Node buildMenuBar() {
+        MenuBar menuBar =new MenuBar();
+        return menuBar;
+    }
+
+    private Node buildNewScene() {
+        TabPane tabPane = new TabPane();
+        return tabPane;
     }
 }
