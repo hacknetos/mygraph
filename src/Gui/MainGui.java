e3a6d655731f355e228ec5a9dd5689aeeb38dedc
hacknetos <76149789+hacknetos@users.noreply.github.com>
2022-01-19 11:34:21 +0100

    new Class and the beginning

---
+++ src/Gui/MainGui.java
@@ -0,0 +1,11 @@
+package Gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainGui extends Application {
+    @Override
+    public void start(Stage primaryStage){
+
+    }
+}
--- src/MAin.java
+++ src/MAin.java
@@ -1,2 +1,7 @@
+import javafx.application.Application;
+import Gui.MainGui;
 public class MAin {
+        public static void main(String[] args) {
+                Application.launch(MainGui.class,args);
+        }
         }
