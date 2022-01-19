28d5012e26279b077c93ff415553b58e4ef13113
hacknetos <76149789+hacknetos@users.noreply.github.com>
2022-01-19 13:05:44 +0100

    Bankmoyn Tab

--- src/Gui/MainGui.java
+++ src/Gui/MainGui.java
@@ -5,14 +5,16 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.Tooltip;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import Gui.Numberfield;
+
 public class MainGui extends Application {
+    Numberfield input;
+    String selectetunter;
     @Override
     public void start(Stage primaryStage){
         BorderPane mainpaPane = new BorderPane();
@@ -41,6 +43,35 @@ public class MainGui extends Application {
 
     private Node buildBankmonyTapContent() {
         BorderPane borderPane = new BorderPane();
+       //TODO borderPane.setCenter(buildBankmonyGraph());
+        borderPane.setLeft(buldLeftborderpane());
+        return borderPane;
+    }
+
+    private Node buldLeftborderpane() {
+        VBox vBox = new VBox(15);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.getChildren().addAll(buidinputfildUnternemen(),buildInputFild(),buildHinzufügenbutton());
+        return vBox;
+    }
+
+    private Node buidinputfildUnternemen() {
+        ComboBox<String> comboBox = new ComboBox<String>();
+        comboBox.setTooltip(new Tooltip("Selcet the Comperny"));
+        //TODO Funktionen
+        return comboBox;
+    }
+
+    private Node buildInputFild() {
+        input = new Numberfield();
+        input.setAlignment(Pos.CENTER);
+        return input;
+    }
+
+    private Node buildHinzufügenbutton() {
+        Button btn = new Button("Add");
+        //TODO Funktionen
+        return btn;
     }
 
     private Tab buildUnternemenTab() {
