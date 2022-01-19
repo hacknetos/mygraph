9f272e783bd2f64a3bacb7eac5c83db469310f29
hacknetos <76149789+hacknetos@users.noreply.github.com>
2022-01-19 11:36:15 +0100

    Primarsstage

--- src/Gui/MainGui.java
+++ src/Gui/MainGui.java
@@ -1,11 +1,15 @@
 package Gui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainGui extends Application {
     @Override
     public void start(Stage primaryStage){
-
+        primaryStage.sizeToScene();
+        primaryStage.setTitle("graphs");
+        primaryStage.setScene(new Scene(/*TODO new scen  */));
+        primaryStage.show();
     }
 }
